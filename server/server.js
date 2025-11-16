355483c1b040770c61941229ef050bf544b7ac5e
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-11-16 18:11:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
-import session from "express-session";
-import passport from "./config/passport.js";
-import morgan from "morgan";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import passport from './config/passport.js';
+import morgan from 'morgan';
+import cors from 'cors';
+import MongoStore from 'connect-mongo';
+import { connectDB } from './config/db.js';
 
-import authRoutes from "./routes/authRoutes.js";
-import searchRoutes from "./routes/searchRoutes.js";
-import historyRoutes from "./routes/historyRoutes.js";
+import authRoutes from './routes/authRoutes.js';
+import searchRoutes from './routes/searchRoutes.js';
+import historyRoutes from './routes/historyRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -16,21 +17,22 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://mern-stack-oauth-image-search-2h8d.onrender.com", 
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
 
 app.use(
   session({
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
@@ -38,7 +40,7 @@ app.use(
       secure: isProd,
       httpOnly: true,
       sameSite: isProd ? 'none' : 'lax',
-      maxAge: 1000 * 60 * 60 * 24 * 7
+      maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   })
 );
@@ -46,10 +48,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/api", authRoutes);
-app.use("/api", searchRoutes);
-app.use("/api", historyRoutes);
+app.use('/api', authRoutes);
+app.use('/api', searchRoutes);
+app.use('/api', historyRoutes);
 
-app.get("/health", (req, res) => res.json({ status: "ok" }));
+app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
